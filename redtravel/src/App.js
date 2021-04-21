63d385faab5c2f01a04f257068bce5fd2fa9e0fe
Anibal <aniballajo.at@gmail.com>
2021-04-21 13:23:35 -0500

    prototipo body del index

--- redtravel/src/App.js
+++ redtravel/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./componentes/Header/header";
 import Login from "./componentes/Login/login";
 import Registro from "./componentes/Usuario/Registro";
+import Body from "./componentes/Body/Body"
 
 const App = () => {
   return (
     <div>
       <Header />
       <Login />
+      <Body/>
       <Registro />
     </div>
   );
---
+++ redtravel/src/componentes/Referencias/Referencias.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Referencias.css"
+
+const Referencias = () => {
+    return (
+        <div className="referencias">
+            Los destinos mas buscados!!
+            <div className="destinos">
+                <div className="imagenes" id="cusco">
+                    Cusco!
+                    </div>
+                <div className="imagenes" id="arequipa">
+                    Arequipa!
+                    </div>
+                <div className="imagenes" id="lima">
+                    Lima!
+                    </div>
+                <div className="imagenes" id="puno">
+                    Puno!
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Referencias
