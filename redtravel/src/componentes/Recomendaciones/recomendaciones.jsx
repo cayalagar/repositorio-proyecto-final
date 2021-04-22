d0c0932e5307fb88e4888c5f31c47068752ce286
BrayanCahuana <cahuanabrayan123@gmail.com>
2021-04-21 23:59:04 -0500

    se actualizo las recomendaciones(85%)y responsive

--- redtravel/src/componentes/Recomendaciones/recomendaciones.jsx
+++ redtravel/src/componentes/Recomendaciones/recomendaciones.jsx
@@ -1,66 +1,67 @@
 import React from "react";
-import imgRecomend from "../../assets/imagenes/machupichu.jpg"
-import "./styles_recomendaciones.css"
+import imgRecomend from "../../assets/imagenes/machupichu.jpg";
+import "./styles_recomendaciones.css";
 
 const Recomendaciones = () => {
-    return (
-
-<div className="container-principal">
-<h2>Destinos recomendados por nuestros viajeros</h2>
-<div className="container-recomendacion">
-
-    <div className="container-recomendacion-imagen">
-    <img src={imgRecomend} alt=""/>
-    </div>
-
-    <div className="container-contenido-boton">
-    <div className="container-recomendacion-contenido">
-    <h2 className="ciudad">Lima</h2>
-    <p className="descripcion">El Circuito Mágico del Agua </p>
-    </div>
-    <div className="container-recomendacion-boton">
-    <button>Saber mas</button>
-    </div>
-    </div>
-</div>
-
-<div className="container-recomendacion">
-
-    <div className="container-recomendacion-imagen">
-    <img src={imgRecomend} alt=""/>
-    </div>
-
-    <div className="container-contenido-boton">
-    <div className="container-recomendacion-contenido">
-    <h2 className="ciudad">Lima</h2>
-    <p className="descripcion">El Circuito Mágico del Agua </p>
-    </div>
-    <div className="container-recomendacion-boton">
-    <button>Saber mas</button>
-    </div>
-    </div>
-</div>
-
-<div className="container-recomendacion">
-
-    <div className="container-recomendacion-imagen">
-    <img src={imgRecomend} alt=""/>
-    </div>
-
-    <div className="container-contenido-boton">
-    <div className="container-recomendacion-contenido">
-    <h2 className="ciudad">Lima</h2>
-    <p className="descripcion">El Circuito Mágico del Agua </p>
-    </div>
-    <div className="container-recomendacion-boton">
-    <button>Saber mas</button>
-    </div>
-    </div>
-</div>
-
-</div>
+  return (
 
 
-    )
-}
-export default Recomendaciones
+    
+    <div className="container-principal">
+      <h2>Destinos recomendados por nuestros viajeros</h2>
+      <div className="container-subprincipal">
+        <div className="container-recomendacion">
+          <div className="container-recomendacion-imagen">
+            <img src={imgRecomend} alt="" />
+          </div>
+
+          <div className="container-contenido-boton">
+            <div className="container-recomendacion-contenido">
+              <h1 className="ciudad">Cusco</h1>
+              <p className="descripcion">El Circuito Mágico del Agua </p>
+            </div>
+            <div className="container-recomendacion-boton">
+              <button>Saber mas</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="container-recomendacion">
+          <div className="container-recomendacion-imagen">
+            <img src={imgRecomend} alt="" />
+          </div>
+
+          <div className="container-contenido-boton">
+            <div className="container-recomendacion-contenido">
+              <h1 className="ciudad">Cusco</h1>
+              <p className="descripcion">El Circuito Mágico del Agua </p>
+            </div>
+            <div className="container-recomendacion-boton">
+              <button>Saber mas</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="container-recomendacion">
+          <div className="container-recomendacion-imagen">
+            <img src={imgRecomend} alt="" />
+          </div>
+
+          <div className="container-contenido-boton">
+            <div className="container-recomendacion-contenido">
+              <h1 className="ciudad">Cusco</h1>
+              <p className="descripcion">El Circuito Mágico del Agua </p>
+            </div>
+            <div className="container-recomendacion-boton">
+              <button>Saber mas</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+        
+      
+    </div>
+  );
+};
+export default Recomendaciones;
