0f201210813e918df2235783672985398a24ccf1
Carlos Ayala <cayalasis@gmail.com>
2021-04-22 22:57:14 -0500

    se agrego navbar en eñ header

--- redtravel/src/App.js
+++ redtravel/src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./componentes/Header/header";
 import Login from "./componentes/Login/login";
 import Registro from "./componentes/Usuario/Registro";
-
+import Compra from "./componentes/Compra/Compra";
 const App = () => {
   return (
     <div>
+    <Router>
       <Header />
-      <Login />
-      <Registro />
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/envios">
+          <Compra />
+        </Route>
+        <Route exact path="/alojamiento">
+          <Compra />
+        </Route>
+        <Route exact path="/paquetes">
+          <Registro />
+        </Route>
+        <Route exact path="/registro">
+          <Registro />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 };
