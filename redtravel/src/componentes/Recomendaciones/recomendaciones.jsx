772fa03f6598743b366915dac50dd0aa87462080
BrayanCahuana <cahuanabrayan123@gmail.com>
2021-04-24 18:45:13 -0500

    se añadio un boton

--- redtravel/src/componentes/Recomendaciones/recomendaciones.jsx
+++ redtravel/src/componentes/Recomendaciones/recomendaciones.jsx
@@ -7,26 +7,29 @@ import { recomendacionesjson } from "../../data/db.json";
 export const Recomendaciones = () => {
   return (
     <div className="container-principal">
-      <h2>Destinos recomendados por nuestros viajeros</h2>
+      <h1>Destinos recomendados por nuestros viajeros</h1>
       <div className="container-subprincipal">
         <div className="container-recomendacion">
 
           {recomendacionesjson.map((data) => {
             return (
               <div className="container-recomendacion-imagen">
-                <img src={imgM} alt="imagen" />
+                <img src={data.imagen} alt="imagen" />
                 <div className="container-contenido-boton">
                   <div className="container-recomendacion-contenido">
                     <h1 className="ciudad">{data.titulo}</h1>
                     <p className="descripcion">{data.body}</p>
                   </div>
                   <div className="container-recomendacion-boton">
-                    <button>Saber mas</button>
+                    <button>Saber más</button>
                   </div>
                 </div>
               </div>
             );})}
         </div>
+      </div>
+      <div className="boton">
+        <button>Ver Todos</button>
       </div>
     </div>
   );
