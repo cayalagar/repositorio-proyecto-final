6552ad4cde419ff2bf82dc362857044d0edaeb71
Carlos Ayala <cayalasis@gmail.com>
2021-04-25 13:55:32 -0500

    modulo de compra al 90%

---
+++ redtravel/src/componentes/Compra/Bus/Bus_Compra.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+/*import React, { useState,useEffect } from "react";*/
+/*import useFetch from "../../hooks/useFetch/useFetch";*/
+
+import "./styles_Bus_Compra.css";
+
+import BusFrente from "../../../assets/imagenes/bus/bus_toma_aeria_frente.jpg"
+import BusAtras from "../../../assets/imagenes/bus/bus_toma_aeria_atras.jpg"
+
+const jsonAsientos = {
+    asientos: [
+        {
+            id: "1",
+            columna: "1",
+            fila: "1",
+            nro_asiento: "01",
+            libre: "true",
+        },
+        {
+            id: "2",
+            columna: "2",
+            fila: "1",
+            nro_asiento: "02",
+            libre: "true",
+        },
+        {
+            id: "3",
+            columna: "3",
+            fila: "1",
+            nro_asiento: "03",
+            libre: "true",
+        },
+        {
+            id: "4",
+            columna: "4",
+            fila: "1",
+            nro_asiento: "04",
+            libre: "true",
+        },
+        {
+            id: "5",
+            columna: "1",
+            fila: "2",
+            nro_asiento: "05",
+            libre: "false",
+        },
+        {
+            id: "6",
+            columna: "2",
+            fila: "2",
+            nro_asiento: "06",
+            libre: "false",
+        },
+        {
+            id: "7",
+            columna: "3",
+            fila: "2",
+            nro_asiento: "07",
+            libre: "false",
+        },
+        {
+            id: "8",
+            columna: "4",
+            fila: "2",
+            nro_asiento: "08",
+            libre: "true",
+        },
+        {
+            id: "9",
+            columna: "1",
+            fila: "3",
+            nro_asiento: "09",
+            libre: "false",
+        },
+        {
+            id: "10",
+            columna: "2",
+            fila: "3",
+            nro_asiento: "10",
+            libre: "true",
+        },
+    ],
+};
+
+const BusAsientos = () => {
+    return (
+        <div className="cont-asiento">
+            <img src={BusFrente} className="imgBus" />
+            <form>
+                <div className="cont-columna">
+                    <input type="submit" className="bto_asiento_ocupado" value="01" />
+                    <input type="submit" className="bto_asiento" value="05" />
+                    <input type="submit" className="bto_asiento" value="09" />
+                    <input type="submit" className="bto_asiento_ocupado" value="13" />
+                </div>
+                <div className="cont-columna">
+                    <input type="submit" className="bto_asiento_ocupado" value="02" />
+                    <input type="submit" className="bto_asiento_ocupado" value="06" />
+                    <input type="submit" className="bto_asiento" value="10" />
+                    <input type="submit" className="bto_asiento_ocupado" value="14" />
+                </div>
+                <div className="cont-columna">
+                    <input type="submit" className="bto_asiento" value="03" />
+                    <input type="submit" className="bto_asiento" value="07" />
+                    <input type="submit" className="bto_asiento" value="11" />
+                    <input type="submit" className="bto_asiento" value="15" />
+                </div>
+                <div className="cont-columna">
+                    <input type="submit" className="bto_asiento_ocupado" value="04" />
+                    <input type="submit" className="bto_asiento_ocupado" value="08" />
+                    <input type="submit" className="bto_asiento" value="12" />
+                    <input type="submit" className="bto_asiento" value="16" />
+                </div>
+            </form>
+            <img src={BusAtras} className="imgBus"/>
+        </div>
+    );
+};
+
+export default BusAsientos;
--- redtravel/src/componentes/Compra/Compra.jsx
+++ redtravel/src/componentes/Compra/Compra.jsx
@@ -1,61 +1,79 @@
 import React from "react";
-import MapCompra from "../CompraMap/CompraMap.jsx"
+import MapCompra from "../CompraMap/CompraMap.jsx";
+import TabsCompra from "./Tabs/Tabs_Compra.jsx";
+import BusAsientos from "./Bus/Bus_Compra.jsx";
 
-import logoVisa from "../../assets/imagenes/medios_de_pago/visa.png"
-import logoPayPal from "../../assets/imagenes/medios_de_pago/paypal.png"
-import logoPagEfe from "../../assets/imagenes/medios_de_pago/pago-efectivo.svg"
+import logoVisa from "../../assets/imagenes/medios_de_pago/visa.png";
+import logoPayPal from "../../assets/imagenes/medios_de_pago/paypal.png";
+import logoPagEfe from "../../assets/imagenes/medios_de_pago/pago-efectivo.svg";
 
 import "./styles_Compra.css";
 
-const mapURL=`https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDWrY6R5VcAbKfY6sYRU2WhQuD1wEWq_ZQ`
+const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDWrY6R5VcAbKfY6sYRU2WhQuD1wEWq_ZQ`;
 
 const Compra = () => {
   return (
     <div className="contenedor-compra">
-            <MapCompra 
-                googleMapURL={mapURL}
-                containerElement={<div className="contenedor-mapa" />}
-                mapElement={<div style={{height:'100%'}}  />} 
-                loadingElement={<p>Cargando</p>}
-            />
+      <div className="contenedor-compra-det">
+        <MapCompra
+          googleMapURL={mapURL}
+          containerElement={<div className="contenedor-mapa" />}
+          mapElement={<div style={{ height: "100%" }} />}
+          loadingElement={<p>Cargando</p>}
+        />
         <div className="contenedor-destino">
-            <div className="contenedor-destino-det">
-                <h1 className="contenedor-destino-det-titulo">LIMA - AREQUIPA</h1>
-                Destino: 1000km <br />
-                Tiempo Estimado: 17h <br />
-                Fecha de Salida: 20/04/2021 <br />
-                Tipo de Viaje: Solo ida <br />
-                N° de Asiento: -- <br />
+          <div className="contenedor-destino-det">
+            <h1 className="contenedor-destino-det-titulo">LIMA - AREQUIPA</h1>
+            Destino: 1000km <br />
+            Tiempo Estimado: 17h <br />
+            Fecha de Salida: 20/04/2021 <br />
+            Tipo de Viaje: Solo ida <br />
+            N° de Asiento: -- <br />
+          </div>
+          <div className="contenedor-destino-form">
+            <form class="fromCompra">
+              <label>Seleccione tipo de servicio:</label>
+              <div class="radio">
+                <input type="radio" name="tipoBoleto" id="standar" />
+                <label for="standar">Standar</label>
+                <input type="radio" name="tipoBoleto" id="vip" />
+                <label for="vip">VIP</label> <br />
+              </div>
+              <div class="contenedor-destino-form-div">
+                <div class="contenedor-destino-form-div-text">
+                  <label name="costoBoleto"> Costo del Boleto: </label>
+                  <input type="text" className="txtcompra" name="costoBoleto" />
+                </div>
+                <div class="contenedor-destino-form-div-text">
+                  <label>Cantidad de asientos:</label>
+                  <input type="number" className="txtcompra" min="1" max="10" />
+                </div>
+                <div class="contenedor-destino-form-div-text">
+                  <label name="costoTotal"> Costo del Boleto: </label>
+                  <input type="text" className="txtcompra" name="costoTotal" />
+                </div>
+                <div className="contenedor-bottom">
+                  <button className="botom-compra" type="submit" value="Submit">
+                    Comprar
+                  </button>
+                </div>
+              </div>
+            </form>
+            <div className="contendor-img-mp">
+              <img src={logoPagEfe} className="imgMP" />
+              <img src={logoPayPal} className="imgMP" />
+              <img src={logoVisa} className="imgMP" />
             </div>
-            <div className="contenedor-destino-form">
-                <div class="wrap">
-                    <form class="fromCompra">
-                        <label>Seleccione tipo de servicio:</label>
-                        <div class="radio">             
-                            <input type="radio" name="tipoBoleto" id="standar" />
-                            <label for="standar">Standar</label>
-                            <input type="radio" name="tipoBoleto" id="vip" />
-                            <label for="vip">VIP</label> <br />
-                        </div>
-                        <label name="costoBoleto"> Costo del Boleto: </label>
-                        <input type="text"  name="costoBoleto"/> <br />
-                        <label>Cantidad de asientos:</label>
-                        <input type="NUMBER" min="1" max="10" value="1" /> <br />
-                        <label name="costoTotal"> Costo del Boleto: </label>
-                        <input type="text"  name="costoTotal"/> <br />
-                        <div className="contenedor-bottom">
-                            <button className="botom-compra" type="submit" value="Submit">Comprar</button>
-                        </div>
-                    </form>
-                </div>                   
-                    <div className="contendor-img-mp">
-                            <img src={logoPagEfe} className="imgMP" />
-                            <img src={logoPayPal} className="imgMP" />
-                            <img src={logoVisa} className="imgMP" />
-                    </div>
-
-            </div>            
+          </div>
         </div>
+      </div>
+      <div className="Contendor-tab">
+        <TabsCompra />
+      </div>
+      <div className="contenedor_asientos">
+        <h2>Selecciona tu asiento's</h2>
+        <BusAsientos />
+      </div>
     </div>
   );
 };
